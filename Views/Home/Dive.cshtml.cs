a16f1b86be6e7d471099daf6a4d7a275460ff39e
Hannah Elliott <e7hannah@gmail.com>
2022-02-16 16:04:17 -0500

    added log a dive to menu bar

---
+++ Views/Home/Dive.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Scuba_Dive_Log.Views.Home
+{
+    public class DiveModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
